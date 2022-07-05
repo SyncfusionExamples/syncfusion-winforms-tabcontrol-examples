19e2d5a028ddd502b943d813f1d677775749532d
RaghavSudhakar <raghavendra.sf3420@gmail.com>
2022-07-05 16:26:44 +0530

    WPF-63696 Created Sample for TabControlAdv.

---
+++ Sample/TabControlAdv_Sample/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Syncfusion.Windows.Forms.Tools;
+using Syncfusion.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace TextBoxExt_BackColor
+{
+    public partial class Form1 : MetroForm
+    {
+        #region Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            //To enable the HotTrack. It helps to change the appearance of the TabControlAdv on mouse hover.
+            this.tabControlAdv1.HotTrack = true;
+            //To draw the Tab item
+            this.tabControlAdv1.DrawItem += new DrawTabEventHandler(tabControlAdv1_DrawItem);
+        }
+        #endregion
+
+        #region Events
+        void tabControlAdv1_DrawItem(object sender, DrawTabEventArgs drawItemInfo)
+        {
+            //To check whether the HotTracking is in progress
+            if ((drawItemInfo.State & DrawItemState.HotLight) > 0)
+            {
+                drawItemInfo.ForeColor = Color.Red;
+                drawItemInfo.BackColor = Color.Khaki;
+                drawItemInfo.Font = new Font("Segoe Marker", 8.0F, FontStyle.Bold);
+            }
+            //To draw the default background and interior
+            drawItemInfo.DrawBackground();
+            drawItemInfo.DrawInterior();
+        }
+        #endregion
+    }
+}
